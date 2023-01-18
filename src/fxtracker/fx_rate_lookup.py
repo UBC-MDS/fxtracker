f96fb43e316d5b21956dc22eea12c5c96cb8006d
Markus Nam <chnam@student.ubc.ca>
2023-01-17 22:40:37 -0800

    feat: implement fx_rate_lookup()

--- src/fxtracker/fx_rate_lookup.py
+++ src/fxtracker/fx_rate_lookup.py
@@ -3,7 +3,7 @@
 
 import datetime
 import pandas as pd
-import pandas_datareader as pdr
+# import pandas_datareader as pdr
 import altair as alt
 import numpy as np
 import yfinance as yf
@@ -22,13 +22,26 @@ def fx_rate_lookup(curr, target_px):
     
     Returns
     --------
-    date
-        The closest date on which the target price falling between day high and day low.
+    date: string
+        The closest date in YYYY-MM-DD on which the target price falling between day high and day low.
 
     
     Examples
     --------
     >>> fx_rate_lookup('EURUSD', 1.072)
-        datetime.date(2023, 1, 10)
+        '2023-01-10'
     """
-    pass
+    
+    df = yf.download(curr + '=X', progress=False, show_errors=False)
+    if len(df) == 0:
+        raise Exception('No data found from data source. Check your ticker.')
+    
+    # A query to check if the input price is within the High and Low of any previous days.
+    myquery = 'High >= ' + str(target_px) + 'and Low <= ' + str(target_px)
+    mydates = df.query(myquery).index.format()
+    
+    # Found, return the latest one (closest to the query date)
+    if len(mydates) > 0:
+        return mydates[-1]
+    else:
+        raise Exception('Target price not found. Adjust your target price.')
