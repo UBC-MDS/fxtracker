153bd193d5f638d280106ad0a9c131d880099711
lennonay <lennon.auyeung@me.com>
2023-01-21 14:31:03 -0800

    updated dependencies

--- src/fxtracker/pl_trend_viz.py
+++ src/fxtracker/pl_trend_viz.py
@@ -10,7 +10,7 @@ from yfinance import shared
 import altair_viewer
 alt.renderers.enable('mimetype')
 
-def pl_trend_viz(curr, start_date, end_date):
+def pl_trend_viz(curr, start_date, end_date, chart_type):
     """
     Visualizes trend of the profit and loss of a currency pair between the selected start date and end date.
     
@@ -22,7 +22,8 @@ def pl_trend_viz(curr, start_date, end_date):
         Start date of the selected period of time
     end_date : string
         End date of the selected period of time
-
+    chart_type: string
+        Type of visulization
     
     Returns
     --------
@@ -30,29 +31,30 @@ def pl_trend_viz(curr, start_date, end_date):
     
     Examples
     --------
-    >>> pl_trend_viz('EURUSD', '2018-12-31', '2022-12-31')
+    >>> pl_trend_viz('EURUSD', '2018-12-31', '2022-12-31','line')
     """
     shared._ERRORS = {}
     curr_X = curr +'=X'
     fx = yf.Ticker(curr_X)
     
-    # Assert start date input value
-    format = "%Y-%m-%d"
-    try: datetime.datetime.strptime(start_date, format)
-    except ValueError:
-        raise ValueError("You have entered an invalid start date! Try date formatted in YYYY-MM-DD.")
+    if not isinstance(curr, str):
+        raise TypeError("curr needs to be of str type.")
+        
+    # Check input type of start_date     
+    if not isinstance(start_date, str):
+        raise TypeError("start_date needs to be of str type.")
+        
+    # Check input type of end_date  
+    if not isinstance(end_date, str):
+        raise TypeError("end_date needs to be of str type.")
     
-    # Assert end date input value
-    try: datetime.datetime.strptime(end_date, format)
-    except ValueError:
-        raise ValueError("You have entered an invalid end date! Try date formatted in YYYY-MM-DD.")
+    if chart_type not in ['line', 'area']:
+        raise TypeError("Your option of plotting should be from 'line' or 'area'")
 
     # Assert end date is later than start date
     format = "%Y-%m-%d"
     if(datetime.datetime.strptime(end_date, format) < datetime.datetime.strptime(start_date, format)):
         raise ValueError("You have entered an end date which is earlier than the start date! Try again.")
-    
-    #date tests adopted from pystockwatch
     
     data = yf.download(curr_X, start_date, end_date)
     if curr_X in shared._ERRORS and 'symbol may be delisted' in shared._ERRORS[curr_X] :
@@ -70,11 +72,18 @@ def pl_trend_viz(curr, start_date, end_date):
     
     title = 'Price trend over time for currency ' + curr
     
-    chart = alt.Chart(data.reset_index(),title = title).mark_line().encode(
-        x = alt.X('Date'),
-        y = alt.Y('Percentage Change',axis=alt.Axis(format='%')),
-        tooltip = alt.Tooltip('Percentage Change',format = '.2%')
-    )
+    if chart_type == 'line':
+        chart = alt.Chart(data.reset_index(),title = title).mark_line().encode(
+            x = alt.X('Date'),
+            y = alt.Y('Percentage Change',axis=alt.Axis(format='%')),
+            tooltip = alt.Tooltip('Percentage Change',format = '.2%')
+        )
+    elif chart_type == 'area':
+        chart = alt.Chart(data.reset_index(),title = title).mark_area().encode(
+            x = alt.X('Date'),
+            y = alt.Y('Percentage Change',axis=alt.Axis(format='%')),
+            tooltip = alt.Tooltip('Percentage Change',format = '.2%')
+        )
     
     chart.show()
     
