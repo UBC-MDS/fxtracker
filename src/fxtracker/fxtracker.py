00ad296f5ea1e66d5a35f410ef0b761b522656d8
lennonay <lennon.auyeung@me.com>
2023-01-27 12:54:40 -0800

    Revert 'more linting'
    This reverts commit 2000a77bb3d80090d798ca9f1f56a888cd1f8677

--- src/fxtracker/fxtracker.py
+++ src/fxtracker/fxtracker.py
@@ -11,8 +11,7 @@ alt.renderers.enable('mimetype')
 
 def fx_rate_lookup(curr, target_px):
     """
-    Return the first date (reverse chronological order) on which the target price 
-    falling between day high and day low based on the availability of data.
+    Return the first date (reverse chronological order) on which the target price falling between day high and day low based on the availability of data.
 
     Parameters
     ----------
@@ -24,8 +23,7 @@ def fx_rate_lookup(curr, target_px):
     Returns
     --------
     date: string
-        The closest date in YYYY-MM-DD on which the target price 
-        falling between day high and day low.
+        The closest date in YYYY-MM-DD on which the target price falling between day high and day low.
 
     Examples
     --------
@@ -59,8 +57,7 @@ def fx_rate_lookup(curr, target_px):
 
 def pl_trend_viz(curr, start_date, end_date, chart_type):
     """
-    Visualizes trend of the profit and loss of a currency pair 
-    between the selected start date and end date.
+    Visualizes trend of the profit and loss of a currency pair between the selected start date and end date.
 
     Parameters
     ----------
@@ -75,8 +72,7 @@ def pl_trend_viz(curr, start_date, end_date, chart_type):
 
     Returns
     --------
-    Line plot that shows the trend of the profit and loss of 
-    a currency pair over the selected period of time
+    Line plot that shows the trend of the profit and loss of a currency pair over the selected period of time
 
     Examples
     --------
@@ -105,10 +101,9 @@ def pl_trend_viz(curr, start_date, end_date, chart_type):
 
     # Assert end date is later than start date
     format = "%Y-%m-%d"
-    if( datetime.datetime.strptime(end_date, format) < 
-       datetime.datetime.strptime(start_date, format)):
+    if(datetime.datetime.strptime(end_date, format) < datetime.datetime.strptime(start_date, format)):
         raise ValueError(
-            "The end date is earlier than the start date! Try again.")
+            "You have entered an end date which is earlier than the start date! Try again.")
 
     data = yf.download(curr_X, start_date, end_date)
 
@@ -150,8 +145,7 @@ def pl_trend_viz(curr, start_date, end_date, chart_type):
 
 def price_trend_viz(curr, start_date, end_date, option):
     """
-    Visualizes trend of the exchange rate of a currency pair
-    between the selected start date and end date.
+    Visualizes trend of the exchange rate of a currency pair between the selected start date and end date.
 
     Parameters
     ----------
@@ -166,8 +160,7 @@ def price_trend_viz(curr, start_date, end_date, option):
 
     Returns
     --------
-    Line plot that shows the trend of the exchange rate
-    of a currency pair over the selected period of time
+    Line plot that shows the trend of the exchange rate of a currency pair over the selected period of time
 
     Examples
     --------
@@ -191,8 +184,7 @@ def price_trend_viz(curr, start_date, end_date, option):
             "Your option of plotting should be from 'Open', 'High', 'Low' or 'Close'")
 
     # Check if the end date entered is later than 2003-12-01
-    if(datetime.datetime.strptime(end_date, "%Y-%m-%d") <
-       datetime.datetime.strptime('2003-12-01', "%Y-%m-%d")):
+    if(datetime.datetime.strptime(end_date, "%Y-%m-%d") < datetime.datetime.strptime('2003-12-01', "%Y-%m-%d")):
         raise Exception("No data exists before 2003-12-01, please try again.")
 
     # Check if the start date entered is earlier than or equal to today
@@ -231,8 +223,7 @@ def price_trend_viz(curr, start_date, end_date, option):
 
 def fx_conversion(curr1, curr2, amt):
     """
-    Converts a specific amount of money from
-    current currency (curr1) to desired currency (curr2)
+    Converts a specific amount of money from current currency (curr1) to desired currency (curr2)
 
     Parameters
     ----------
